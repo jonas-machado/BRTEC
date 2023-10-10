3fc284105a43f145c1fc7cd81b0ac63570f2491d
Jonas <jonas.m.a.1997@gmail.com>
2023-10-10 01:35:35 -0300

    done

--- components/navLink/Nav.tsx
+++ components/navLink/Nav.tsx
@@ -10,6 +10,7 @@ import {
 } from "@heroicons/react/20/solid";
 import Link from "next/link";
 import { motion, useDragControls } from "framer-motion";
+import Image from "next/image";
 
 const home = {
   name: "Config",
@@ -36,48 +37,53 @@ const Nav = ({ classname }: { classname: string }) => {
   const [showNav, setShowNav] = useState(false);
   return (
     <div
-      className="fixed left-1/2 -translate-x-1/2 z-50 "
+      className="fixed bottom-16 left-1/2 -translate-x-1/2 z-50 w-full"
       onMouseEnter={() => setIsShowing(true)}
       onMouseLeave={() => setShowNav(false)}
     >
-      <Transition
-        show={!isShowing}
-        enter="transition ease-out duration-200 delay-[400ms]"
-        enterFrom="opacity-0 translate-y-6"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-6"
-        afterLeave={() => setShowNav(true)}
-      >
-        <ChevronDoubleUpIcon className="h-14 w-14 text-gray-950" />
-      </Transition>
-      <Transition
-        show={showNav}
-        enter="transition ease-out duration-200 delay-[400ms]"
-        enterFrom="opacity-0 translate-y-6"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-100"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-6"
-        afterLeave={() => setIsShowing(false)}
-      >
-        <div className="flex bg-gray-800 items-center rounded-sm h-10 p-4 pl-0 shadow-md shadow-black border-gray-900 border-2">
-          <Link
-            href={home.link}
-            className="flex  rounded-md bg-gray-900 p-2 items-center mr-2 shadow-lg shadow-black"
-          >
-            <HomeIcon className="w-10 h-10 text-gray-300" />
-          </Link>
-          <ul className="flex gap-4">
-            {links.map((link) => (
-              <li key={link.name} className="text-gray-300 hover:text-gray-50">
-                <Link href={link.link}>{link.name}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </Transition>
+      <div className="flex justify-center">
+        <Transition
+          show={!isShowing}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0 translate-y-6"
+          enterTo="opacity-100 translate-y-0"
+          leave="transition ease-in"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-6"
+          afterLeave={() => setShowNav(true)}
+        >
+          <ChevronDoubleUpIcon className="h-14 w-14 text-gray-950" />
+        </Transition>
+        <Transition
+          show={showNav}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0 translate-y-6"
+          enterTo="opacity-100 translate-y-0"
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-6"
+          afterLeave={() => setIsShowing(false)}
+        >
+          <div className="flex bg-gray-800 items-center rounded-sm h-10 p-4 pl-0 shadow-md shadow-black border-gray-900 border-2">
+            <Link
+              href={home.link}
+              className="flex  rounded-md bg-gray-900 p-2 items-center mr-2 shadow-lg shadow-black"
+            >
+              <HomeIcon className="w-10 h-10 text-gray-300" />
+            </Link>
+            <ul className="flex gap-4">
+              {links.map((link) => (
+                <li
+                  key={link.name}
+                  className="text-gray-300 hover:text-gray-50"
+                >
+                  <Link href={link.link}>{link.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Transition>
+      </div>
     </div>
   );
 };
