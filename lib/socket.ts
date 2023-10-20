24ab1120517375e4b0f73f0453afd2747a45cac7
Jonas <jonas.m.a.1997@gmail.com>
2023-10-20 13:50:24 -0300

    done

--- lib/socket.ts
+++ lib/socket.ts
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL: any =
   process.env.NODE_ENV === "production"
-    ? undefined
-    : "http://177.200.131.54:3001";
+    ? "http://177.200.131.54:3001"
+    : "http://localhost:3001";
 
 export const socket = io(URL);
