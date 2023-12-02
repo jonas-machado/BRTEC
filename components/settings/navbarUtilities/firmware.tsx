9c8fda2a1134ac797f885101087a4085fd52b9ef
Jonas <jonas.m.a.1997@gmail.com>
2023-12-02 15:30:28 -0300

    ...

--- components/settings/navbarUtilities/firmware.tsx
+++ components/settings/navbarUtilities/firmware.tsx
@@ -112,21 +112,7 @@ export default function Firmware({ firmware }: any) {
                 <div className="flex gap-2">
                   <div className="hidden shrink-0 sm:flex items-center gap-2 w-full">
                     <button className="bg-gray-800 text-gray-300 p-2 rounded-md w-full hover:bg-gray-700 transition">
-                      Editar
-                    </button>
-                    <button className="bg-gray-800 text-gray-300 p-2 rounded-md w-full hover:bg-gray-700 transition">
-                      {deleteLoading?.state && deleteLoading?.index == i ? (
-                        <div
-                          className="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                          role="status"
-                        >
-                          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                            Loading...
-                          </span>
-                        </div>
-                      ) : (
-                        "Excluir"
-                      )}
+                      Excluir
                     </button>
                   </div>
                 </div>
