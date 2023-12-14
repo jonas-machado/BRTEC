67291afd79223bcbe469675da6157857df45872c
Jonas <jonas.m.a.1997@gmail.com>
2023-12-14 00:22:31 -0300

    dashboard loaded

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import LoginForm from "../../components/form/LoginForm";
 
 export default async function LoginPage() {
   return (
     <>
+      <Image
+        src={`/images/bg1.gif`}
+        alt="bg"
+        fill
+        className="-z-50 bg-no-repeat"
+        blurDataURL={`/images/bg1.gif`}
+      />
       <div
-        className={`h-screen flex items-center justify-center bg-[url('/images/bg1.gif')] bg-no-repeat bg-fixed bg-cover`}
+        className={`h-screen flex items-center justify-center bg-no-repeat bg-fixed bg-cover`}
         id="login"
       >
         <LoginForm />
