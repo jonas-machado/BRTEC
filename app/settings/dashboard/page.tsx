f3e9f0fc1ef45457b4881afa2895e8aa94d317dd
Jonas <jonas.m.a.1997@gmail.com>
2023-12-19 08:03:44 -0300

    to server

--- app/settings/dashboard/page.tsx
+++ app/settings/dashboard/page.tsx
@@ -1,12 +1,7 @@
 //import io from "socket.io-client";
-import { getOlt } from "@/lib/actions/getOlt";
-import ConfigForm from "@/components/form/ConfigForm";
-import getCurrentUser from "@/lib/actions/getCurrentUser";
-import MotionComponent from "@/lib/framerMotion/motionComponent";
-import Settings from "@/components/settings/Sidebar";
+
 import Dashboard from "@/components/settings/Dashboard";
 import getUsers from "@/lib/actions/getUsers";
-import dynamic from "next/dynamic";
 
 export default async function DashboardPage() {
   const users = await getUsers();
