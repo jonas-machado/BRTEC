4873073d5aeb1cdbe58c93d6c436e033b283879b
Jonas <jonas.m.a.1997@gmail.com>
2023-12-30 15:24:04 -0300

    adding pusher

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
+  webpack: (config) => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+
+    config.plugins.push(new webpack.DefinePlugin(env));
+
+    return config;
+  },
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
 };
-
-module.exports = nextConfig;
