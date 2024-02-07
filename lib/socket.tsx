d998fc53b838dd13c47d8870f1ba333cbba18df2
Jonas <jonas.m.a.1997@gmail.com>
2024-02-07 09:48:08 -0300

    ...

--- lib/socket.tsx
+++ lib/socket.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { io, Socket } from "socket.io-client";
 import React from "react";
-const URL: any = "http://localhost:3001";
+//const URL: any = "http://localhost:3001";
 
 // "undefined" means the URL will be computed from the `window.location` object
-// const URL: any =
-//   process.env.NODE_ENV === "production"
-//     ? "http://177.200.131.54:3001"
-//     : "http://localhost:3001";
+const URL: any =
+  process.env.NODE_ENV === "production"
+    ? "http://177.200.131.54:3001"
+    : "http://localhost:3001";
 
 const socket = io(URL);
 const SocketContext = React.createContext(socket);
